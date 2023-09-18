5690f590a193e4ff2d6791ea5c04443d28f85d0c
platisam <platisam@gmail.com>
2023-09-18 17:35:20 +0200

    adding Route for each page

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import "./index.css";
 import App from "./App";
 import { AppProvider } from "./context";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <AppProvider>
       <App />
     </AppProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
