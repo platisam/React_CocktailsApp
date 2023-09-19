d5954953f9e233f5c163b4dc792f5099525cea32
platisam <platisam@gmail.com>
2023-09-19 14:31:54 +0200

    setting About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,16 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas
+        deleniti voluptatem minima laborum eos dolorem, voluptatibus amet neque
+        debitis fugit iusto labore repudiandae, quae consectetur nisi ratione
+        voluptate, numquam dolor quod? Fugiat accusamus porro, dolorum alias
+        repellat numquam eveniet facilis?
+      </p>
+    </section>
   );
 };
 export default About;
