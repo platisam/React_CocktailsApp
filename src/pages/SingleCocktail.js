3cec6ea396b25005780ee696f71bf1d8f38bf639
platisam <platisam@gmail.com>
2023-09-20 12:41:07 +0200

    setting multiple returns to SingleCocktail page

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import Loading from "../components/Loading";
 const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
 
 const SingleCocktail = () => {
@@ -54,11 +55,23 @@ const SingleCocktail = () => {
     }
     getCocktail();
   }, [id]);
+  if (loading) {
+    return <Loading />;
+  }
+  if (!cocktail) {
+    return <h2 className="section-title">no cocktail to display</h2>;
+  }
+
+  const { name, image, info, category, glass, instructions, ingredients } =
+    cocktail;
 
   return (
-    <div>
-      <h2>{id}</h2>
-    </div>
+    <section className="section cocktail-section">
+      <Link to="/" className="btn btn-primary">
+        back home
+      </Link>
+      <h2 className="section-title">{name}</h2>
+    </section>
   );
 };
 export default SingleCocktail;
